1d838809eb3da1d89779b5d195b1334ad3d5e529
sanika chavan <sanikac10@gmail.com>
2021-10-22 09:11:04 +0530

    Initial Commit

---
+++ Guess the Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by sanika chavan on 22/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
