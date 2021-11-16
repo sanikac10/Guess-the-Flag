ecda07e36163f533f8095b054b8dee3cf896876f
sanika chavan <sanikac10@gmail.com>
2021-11-16 20:18:25 +0530

    Final

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -6,11 +6,89 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    var scoreCard = 0
+    var finalScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if #available(iOS 15.0, *) {
+            ZStack{
+                RadialGradient(stops: [
+                    .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                    .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                        
+                ], center: .top,startRadius: 200, endRadius: 700)
+                    .ignoresSafeArea()
+                VStack{
+                    Spacer()
+                    
+                    Text("Guess the Flag!")
+                        .font(.largeTitle.weight(.bold))
+                        .foregroundColor(.white)
+                    VStack(spacing: 15) {
+                        VStack{
+                            Text("Tap the flag of").foregroundStyle(.secondary)
+                                .font(.subheadline.weight(.heavy))
+                            Text(countries[correctAnswer])
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        ForEach(0..<3) { number in
+                            Button {
+                                flagTapped(number)
+                                
+                            } label: {
+                                Image(countries[number]).renderingMode(.original)
+                                    .clipShape(Capsule())
+                                    .shadow(radius: 5)
+                                
+                            }
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    Spacer()
+                    Spacer()
+                    
+                    Text("Score: ???")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+                    
+                    Spacer()
+                }
+                .padding()
+            } .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is: ???")
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    mutating func score(){
+        if scoreTitle == "Correct" {
+            scoreCard = scoreCard + 1
+            finalScore = scoreCard
+    }
     }
 }
 
